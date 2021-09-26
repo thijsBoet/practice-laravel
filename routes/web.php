e108bf9be0cf0be495667f81fb85067f786af232
Matthijs Boet <m.boet@chello.nl>
2021-09-26 13:55:33 +0200

    connected to DB

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home')->name('home');
+
+
+Route::get('/', [HomeController::class, 'home'])
+    ->name('home');
+Route::get('/contact', [HomeController::class, 'contact'])
+    ->name('contact');
+
+Route::get('/single', AboutController::class);
+
+Route::resource('posts', PostController::class)->only(['index', 'show']);
+
+// Route::get('/posts', function () use($posts) {
+//     // dd(request()->all());
+//     dd((int)request()->input('page', 1));
+//     return view('posts', ['posts' => $posts]);
+// });
+
+// Route::get('post/{id}', function ($id) use($posts){
+//     return 'Blog post ' . $id;
+// })->name('post');
+
+Route::view('/about', 'about')->name('about');
 
 $posts = [
     1 => [
@@ -36,30 +61,30 @@ $posts = [
     ]
 ];
 
-Route::get('/posts', function () use($posts) {
-    return view('posts', ['posts' => $posts]);
-});
-
-Route::get('post/{id}', function ($id) use($posts){
-    return 'Blog post ' . $id;
-})->name('post');
-
-Route::view('/about', 'about')->name('about');
-
-Route::view('/contact', 'contact')->name('contact');
-
-Route::prefix('/re')->name('res.')->group(function() use($posts) {
+Route::prefix('/re')->name('res.')->group(function () use ($posts) {
     Route::get('/response', function () use ($posts) {
         return response($posts, 201)
             ->header('Content-Type', 'application/json')
             ->cookie('MY_COOKIE', 'Matthijs Boet', 3600);
     })->name('responses');
 
-    Route::get('/redirect', function() {
-        return redirect('/');
+    Route::get('/redirect', function () {
+        return redirect('/posts', ['id' => 1]);
     })->name('/redirect');
 
     Route::get('/back', function () {
         return back();
     })->name('back');
+
+    Route::get('/named', function () {
+        return redirect()->route('about', ['id' => 1]);
+    });
+
+    Route::get('/away', function () {
+        return redirect()->away("http://yahoo.com");
+    });
+
+    Route::get('/json', function ()  use ($posts) {
+        return response()->json($posts);
+    });
 });
